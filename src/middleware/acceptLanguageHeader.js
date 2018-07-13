6f116b11c465db413f25cbafd4ec442919b447a1
Lorenzo Kappeler <me@lnk.codes>
2018-07-13 14:42:12 +0200

    fix(regex): Fix the language parser regex

--- src/middleware/acceptLanguageHeader.js
+++ src/middleware/acceptLanguageHeader.js
@@ -11,6 +11,10 @@ class LanguageHeaderParser {
         return next();
     }
 
+    parseLanguageHeader(header) {
+        return /^(?:(\*)|(?:([a-z]{2,3})(?:[-_]([a-z]{2,3}))?))(?:;\s*q\s*=\s*((1|0)(\.[0-9]{1,2})?))?$/i.exec(header);
+    }
+
     /**
      *  Parses request forms and assignes properties files and fileds to the
      *  body object,
@@ -23,14 +27,13 @@ class LanguageHeaderParser {
         return headerText
             .split(/\s*,\s*/gi)
             .map((item) => {
-                // eslint-disable-next-line no-useless-escape
-                const parsed = /([a-z]{2}|[\*]{1})[-|_]?([a-z]{2})?(?:\s*;\s*q\s*=\s*([0-9\.]+))?/gi.exec(item);
+                const parsed = this.parseLanguageHeader(item);
                 if (!parsed) return null;
 
                 return {
-                    language: (parsed[1] || '').toLowerCase().trim(),
-                    country: (parsed[2] || '').toLowerCase().trim(),
-                    priority: parsed[3] ? parseFloat(parsed[3]) : 1,
+                    language: (parsed[2] || parsed[1]).toLowerCase(),
+                    country: (parsed[3] || '').toLowerCase(),
+                    priority: parsed[4] ? parseFloat(parsed[4], 10) : 1,
                 };
             })
             .filter(item => item !== null)
